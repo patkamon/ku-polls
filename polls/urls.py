ea9da74683702f24ff44dc81f41a4656c172707c
patkamon <patkamon.a@ku.th>
2021-09-11 23:41:01 +0700

    finish navigate message framework

--- polls/urls.py
+++ polls/urls.py
@@ -3,10 +3,15 @@ from . import views
 
 app_name = 'polls'
 urlpatterns = [
-    path('', views.IndexView.as_view(), name='index'),
-    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    # path('', views.IndexView.as_view(), name='index'),
+    # path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    # path('<int:question_id>/vote/', views.vote, name='vote'),
+    # path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+    path('', views.index, name='index'),
+    path('<int:question_id>/', views.detail, name='detail'),
+    path('<int:question_id>/results/', views.results, name='results'),
     path('<int:question_id>/vote/', views.vote, name='vote'),
-    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+    #images
     ##this path was create to make zingChart
     path('resultdata/<str:obj>/',views.resultData, name= 'resultData'),
 ]

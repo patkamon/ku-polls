94782ddc200fda1cc2fd8ddf99e54367be378fd5
patkamon <patkamon.a@ku.th>
2021-09-14 12:44:10 +0700

    fixing unittest

--- polls/views.py
+++ polls/views.py
@@ -10,41 +10,17 @@ from django.contrib import messages
 
 # Create your views here.
 def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[::-1]
+    latest_question_list = Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[::-1]
     context = {'latest_question_list': latest_question_list}
     return render(request, 'polls/index.html', context)
-    
+
 def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     if question.can_vote():
         return render(request, 'polls/detail.html', {'question': question})
     messages.error(request, "You try to access poll that does not allow")
     return  HttpResponseRedirect(reverse('polls:index'))
-# class IndexView(generic.ListView):
-#     template_name = 'polls/index.html'
-#     context_object_name = 'latest_question_list'
-#
-#     def get_queryset(self):
-#         """
-#         Return the last five published questions (not including those set to be
-#         published in the future).
-#         """
-#         return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[::-1]
 
-# class DetailView(generic.DetailView):
-#     model = Question
-#     template_name = 'polls/detail.html'
-#     def get_queryset(self):
-#         """
-#         Excludes any questions that aren't published yet.
-#         """
-#         if Question.objects.filter().can_vote():
-#             return Question.objects.filter(pub_date__lte=timezone.now())
-
-
-# class ResultsView(generic.DetailView):
-#     model = Question
-#     template_name = 'polls/results.html'
 def results(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/results.html', {'question': question})

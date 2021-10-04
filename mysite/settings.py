31c8b8325cfc4b56c5dc0e534e8b753cd46eacb7
patkamon <patkamon.a@ku.th>
2021-10-04 20:40:56 +0700

    working on travis secretkey

--- mysite/settings.py
+++ mysite/settings.py
@@ -20,8 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
-
+# SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = '5t4(w8!v74bpffvg1#1p-$-ju@rcce(0etgi*x97zn=b^f69h!'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True)
 

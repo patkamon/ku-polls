a76deb46680587aa681d5e2687458d541e346a4b
patkamon <patkamon.a@ku.th>
2021-10-26 22:47:47 +0700

    add authenticate unittest

--- polls/tests/test_auth.py
+++ polls/tests/test_auth.py
@@ -3,17 +3,35 @@ from django.contrib.auth.models import User
 from django.test import TestCase
 from django.urls import reverse
 
+import datetime
+from django.utils import timezone
+from polls.models import Question
+
+
+def create_question(question_text, days, end=1):
+    """
+    Create question mockup.
+
+    Create a question with the given `question_text` and published the
+    given number of `days` offset to now (negative for questions published
+    in the past, positive for questions that have yet to be published).
+    """
+    time = timezone.now() + datetime.timedelta(days=days)
+    end_time = time + datetime.timedelta(days=end)
+    return Question.objects.create(question_text=question_text, pub_date=time, end_date=end_time)
+
+
 class UserAuthTest(TestCase):
 
     def setUp(self):
         super().setUp()
-        self.username = "testuser"
-        self.password = "Fat-Chance!"
+        self.username = "Patlom"
+        self.password = "lompat-rankrank!"
         self.user1 = User.objects.create_user(
                          username=self.username,
                          password=self.password,
-                         email="testuser@nowhere.com")
-        self.user1.first_name = "Tester"
+                         email="lompat@patlom.com")
+        self.user1.first_name = "Patlom-RankMak"
         self.user1.save()
 
     def test_login_view(self):
@@ -29,3 +47,30 @@ class UserAuthTest(TestCase):
         self.assertEqual(302, response.status_code)
         # should redirect us to the polls index page ("polls:index")
         self.assertRedirects(response, reverse("polls:index"))
+        #check is there name of user in index page
+        index_url = reverse("polls:index")
+        index_response = self.client.get(index_url)
+        self.assertContains(index_response, self.username)
+
+    def test_logout_view(self):
+        """Test that there is logout views"""
+        self.client.login(username=self.username, password=self.password)
+        logout_url = reverse("logout")
+        response = self.client.get(logout_url)
+        self.assertContains(response, "Good bye!")
+        self.assertNotContains(response, self.username)
+
+    def test_authenticate_vote(self):
+        """Test that authenticated user be able to vote."""
+        self.client.login(username=self.username, password=self.password)
+        question = create_question(question_text='This is a question', days=-5)
+        response = self.client.get(reverse('polls:vote', args=(question.id,)))
+        self.assertEqual(200, response.status_code)
+
+    def test_non_authenticate_vote(self):
+        """Test the outsider vote."""
+        question = create_question(question_text='This is a question', days=-5)
+        response = self.client.get(reverse('polls:vote', args=(question.id,)))
+        self.assertEqual(302, response.status_code)
+        #redirect to login page
+        self.assertRedirects(response, "/accounts/login/?next=/polls/1/vote/")
